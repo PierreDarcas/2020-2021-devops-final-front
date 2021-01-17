f4a7c8a1d604671eed887c8812bae2fe06ded291
Pierre <pierre.darcas@lacatholille.fr>
2021-01-17 23:26:26 +0100

    :wrench: src/common/config.js 2

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://salty-anchorage-48756.herokuapp.com/api"
+    ? "https://damp-gorge-24987.herokuapp.com/api"
     : "http://localhost:2020/api";
 export default API_URL;
